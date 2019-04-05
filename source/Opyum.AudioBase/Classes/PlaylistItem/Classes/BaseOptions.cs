34af6ffe664973d99eb58e0a9288a90b69e3eeed
malebra <bornos.stojak@hotmail.com>
2019-04-05 19:28:36 +0200

    Edited AudioBase

---
+++ source/Opyum.AudioBase/Classes/PlaylistItem/Classes/BaseOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opyum.AudioBase
+{
+    public class BaseOptions : IOptions
+    {
+    }
+}
---
+++ source/Opyum.AudioBase/Classes/PlaylistItem/PlaylistItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opyum.AudioBase
+{
+    public class PlaylistItem : IPlaylistItem
+    {
+        public IDuration Duration { get; }
+
+        public ITime Time { get;  }
+
+        public ITags Tags { get; }
+
+        public IWatcher Watcher { get; }
+
+
+        public IContent Conntent { get; }
+
+        public IOptions Options { get; }
+
+
+
+    }
+}
